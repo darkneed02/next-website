e2d0d6448dea9884f24cd7a4a7de6ddbfc020b71
Jaruwat Amnuaysat <66204686+darkneed02@users.noreply.github.com>
2023-11-26 16:37:17 +0700

    Frist Project

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image'
+import  {Socials} from '@/constants'
+
+const Navbar = () => {
+  return (
+    <div className='fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20'>
+        <div className='flex flex-row gap-3 items-center'>
+            <div className='relative'>
+            <Image 
+                src='/horseLogo.jpg'
+                alt = 'logo'
+                width = {40}
+                height = {40}
+                className = 'w-full h-full object-contain rounded-full'>
+            </Image>
+            </div>
+            <h1 className='text-white text-[25px] font-semibold'> Webchain Dev</h1>
+        </div>
+
+        <div className='flex flex-row gap-5 mb-2'>
+            {Socials.map((soical) => (
+                <Image 
+                    key={soical.name}
+                    src={soical.src}
+                    alt={soical.name}
+                    width={28}
+                    height={28}
+                />
+            ))}
+        </div>
+       
+    </div>
+  );
+};
+
+export default Navbar
