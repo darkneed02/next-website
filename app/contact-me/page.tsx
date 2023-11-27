956d9e57bb190771b1a56e487665124a9719db4b
Jaruwat Amnuaysat <66204686+darkneed02@users.noreply.github.com>
2023-11-27 15:03:58 +0700

    Contact Me

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import ContactForm from '@/components/ContactForm'
 
 const page = () => {
   return (
-    <div>Contact me</div>
+    <div className='w-screen h-screen bg-cover flex items-center justify-center' style={{backgroundImage : 'url(bg-3.jpg)'}}>
+      <div className='h-[60%] w-[80%] relative bg-cover bg-center rounded-xl border border-white' style={{backgroundImage: 'url(atombg-comp.webp)'}}>
+         <div className='absolute left-20 bottom-16 w-[70%] md:w-[30%]'>
+            <ContactForm />
+         </div>
+      </div>
+    </div>
   )
 }
 
