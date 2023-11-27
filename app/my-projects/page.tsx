cdfd86b5c51bfb5916169cecf15972a8eb6f5039
Jaruwat Amnuaysat <66204686+darkneed02@users.noreply.github.com>
2023-11-27 14:44:07 +0700

    Prject

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Projects } from '@/constants'
+import ProjectCard from '@/components/ProjectCard'
 
 const page = () => {
   return (
-    <div>My Project</div>
+    <div className='w-screen h-screen flex items-center justify-center bg-center bg-cover' style={{backgroundImage: 'url(/mountains.jpg)'}}>
+      <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
+         { Projects.map((project, index)=>(
+            < ProjectCard
+               key={index}
+               title= {project.title}
+               text={project.text}
+               image={project.src}
+            />
+         )) }
+      </div>
+    </div>
   )
 }
 
